dd2c2e2996078721c3b008210805f41008e75aa5
Larabisso <165944327+Larabisso@users.noreply.github.com>
2026-09-15 22:13:10 -0300

    Update Main.java

--- ej3/Main.java
+++ ej3/Main.java
@@ -4,9 +4,6 @@ public class Main {
         new HiloContador("Dos").start();
         new HiloContador("Tres").start();
 
-        // Pregunta para pensar: ¿El orden de las salidas siempre es el mismo?
-        // No, el orden puede variar en cada ejecución. Esto pasa porque el sistema
-        // operativo decide cuándo le toca el turno a cada hilo, y ese orden no está
-        // garantizado ni es siempre igual.
+    
     }
 }
