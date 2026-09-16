421e0bbf296dcdcf845ff5d3ef1a458939d3d859
Larabisso <165944327+Larabisso@users.noreply.github.com>
2026-09-15 22:13:37 -0300

    Update Contador.java

--- ej4/Contador.java
+++ ej4/Contador.java
@@ -1,9 +1,7 @@
 class Contador {
     private int valor = 0;
 
-    // Versión con condición de carrera: sacá "synchronized" de la firma de abajo
-    // para reproducir el problema (el resultado final variará y será menor a 5000).
-    public synchronized void incrementar() { // ¡Aquí está la solución!
+    public synchronized void incrementar() { 
         valor++;
     }
 
